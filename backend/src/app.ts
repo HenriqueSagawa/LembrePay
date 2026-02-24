b0b15c192bebdb2f7d056f2a6e78a42f5eac1eea
Henrique Sagawa <henriquetutomusagawa@gmail.com>
2026-02-24 13:29:14 -0300

    feat(auth app): configure app.ts to receive the auth routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors'
 import { config } from 'dotenv';
+import { authRoutes } from './routes/auth.routes.js';
+import { errorMiddleware } from './middlewares/error.middleware.js';
 
 config();
 
@@ -8,5 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.use(errorMiddleware);
 
 export { app }

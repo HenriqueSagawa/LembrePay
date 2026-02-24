cc1ab6641a36b68c028f546bf22b18bbe657258c
Henrique Sagawa <henriquetutomusagawa@gmail.com>
2026-02-24 10:46:52 -0300

    feat(server): initialize express server

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors'
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+export { app }
